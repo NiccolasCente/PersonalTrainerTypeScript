69b1d8043e38465ceb11ea5d9af97b9a2ff4d232
NiccolasCente <niccolascente@hotmail.com>
2024-09-30 21:56:11 -0300

    header is done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container, Carousel } from 'react-bootstrap';
+import AnimatedSection from '../Hooks/AnimatedSection';
 import './Header.css';
 import '../../assets/GlobalStyles.css';
 
@@ -10,25 +11,42 @@ import Carousel3 from "../../assets/Header/Carousel3.jpg";
 const Header: React.FC = () => {
   return (
     <header className="header-wrapper">
-      <Container fluid>
-        <div className="d-flex justify-content-between align-items-center">
+      <Container fluid className="p-0"> 
+        <Navbar expand="lg" className="header-content">
           <Navbar.Brand href="#home" className="fw-bold text-white gym-logo">
+            <AnimatedSection animation="fade-in">
             Fitness Center<span style={{ color: 'red' }}>.</span>
+            </AnimatedSection>
           </Navbar.Brand>
-
-          <Nav className="mx-auto nav-center">
-            <Nav.Link href="#home" className="text-white">Início</Nav.Link>
-            <Nav.Link href="#about" className="text-white">Sobre</Nav.Link>
-            <Nav.Link href="#gallery" className="text-white">Serviços</Nav.Link>
-            <Nav.Link href="#pricing" className="text-white">Preços</Nav.Link>
-            <Nav.Link href="#pages" className="text-white">Opiniões</Nav.Link>
-            <Nav.Link href="#contact" className="text-white">Contato</Nav.Link>
-          </Nav>
-
-          <div className="align-self-end">
-            <a href="#join" className="btn btn-danger">Junte-se a nós</a>
-          </div>
-        </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-danger" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto nav-center">
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#home" className="text-white">Início</Nav.Link>
+              </AnimatedSection>
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#about" className="text-white">Sobre</Nav.Link>
+              </AnimatedSection>
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#gallery" className="text-white">Serviços</Nav.Link>
+              </AnimatedSection>
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#pricing" className="text-white">Preços</Nav.Link>
+              </AnimatedSection>
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#pages" className="text-white">Opiniões</Nav.Link>
+              </AnimatedSection>
+              <AnimatedSection animation="fade-in">
+              <Nav.Link href="#contact" className="text-white">Contato</Nav.Link>
+              </AnimatedSection>
+            </Nav>
+            <div className="align-self-end">
+            <AnimatedSection animation="fade-in">
+              <a href="#join" className="btn btn-danger">Junte-se a nós</a>
+            </AnimatedSection>
+            </div>
+          </Navbar.Collapse>
+        </Navbar>
 
         <Carousel className="mt-4">
           <Carousel.Item className="carousel-item-custom">
@@ -37,11 +55,21 @@ const Header: React.FC = () => {
               src={Carousel1}
               alt="Primeira Imagem"
             />
+
             <Carousel.Caption className="carousel-caption-custom">
-              <h2>Construa Seu</h2>
-              <h1><strong style={{color:'black'}}>Corpo</strong></h1>
-              <h3>Cosntrua Um Corpo Saúdavel Com Um Toque Profissional</h3>
+            <AnimatedSection animation="fade-in">
+              <h2 className="h2-carousel">Construa Seu</h2>
+            </AnimatedSection>
+            <AnimatedSection animation="fade-in">
+              <h1 className="h1-carousel">Corpo</h1>
+            </AnimatedSection>
+            <AnimatedSection animation="fade-in">
+              <h3 className="h3-carousel">Construa Um Corpo Saudável Com Um Toque Profissional</h3>
+            </AnimatedSection>
+            <AnimatedSection animation="fade-in">
               <a href="#action1" className="btn btn-primary">Junte-se a nós</a>
+            </AnimatedSection>
+
             </Carousel.Caption>
           </Carousel.Item>
 
@@ -52,10 +80,10 @@ const Header: React.FC = () => {
               alt="Segunda Imagem"
             />
             <Carousel.Caption className="carousel-caption-custom">
-              <h2>Título H2 2</h2>
-              <h1>Título H1 2</h1>
-              <h3>Subtítulo H3 2</h3>
-              <a href="#action2" className="btn btn-primary">Ação 2</a>
+              <h2 className="h2-carousel">Equipamentos e Metodologia de Ensino</h2>
+              <h1 className="h1-carousel">Atual para que você progrida</h1>
+              <h3 className="h3-carousel">Os Melhores Professores do Mercado</h3>
+              <a href="#action2" className="btn btn-primary">Junte-se a nós</a>
             </Carousel.Caption>
           </Carousel.Item>
 
@@ -66,10 +94,10 @@ const Header: React.FC = () => {
               alt="Terceira Imagem"
             />
             <Carousel.Caption className="carousel-caption-custom">
-              <h2>Título H2 3</h2>
-              <h1>Título H1 3</h1>
-              <h3>Subtítulo H3 3</h3>
-              <a href="#action3" className="btn btn-primary">Ação 3</a>
+              <h2 className="h2-carousel">Musculação, Alimentação e Disciplina são</h2>
+              <h1 className="h1-carousel">Os 3 Pilares Para Sua Evolução</h1>
+              <h3 className="h3-carousel">Nunca é Tarde Para Mudar De Vida</h3>
+              <a href="#action3" className="btn btn-primary">Junte-se a nós</a>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
